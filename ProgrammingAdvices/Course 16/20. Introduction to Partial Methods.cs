bc0c28848c6c57c3be6f8cd0aecb9b307bec65d4
Mostafa Shraief <mostafashraief5@gmail.com>
2024-11-14 23:45:34 +0300

    Add 'Introduction to Partial Methods' source code with note

---
+++ ProgrammingAdvices/Course 16/20. Introduction to Partial Methods.cs
@@ -0,0 +1,36 @@
+using System;
+
+public partial class clsPerson
+{
+
+    partial void Hi();
+
+    public void CallHi()
+    {
+        Hi();
+    }
+}
+
+public partial class clsPerson
+{
+    partial void Hi()
+    {
+        Console.WriteLine("Hi");
+    }
+}
+
+// Things to remember about Partial Method
+// partial keyword.
+// return type void.
+// implicitly private.
+// and cannot be virtual.
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        clsPerson person = new clsPerson();
+        person.CallHi();
+    }
+}
