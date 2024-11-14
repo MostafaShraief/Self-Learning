e5b603002a8092f499533878782afa7eab056d4a
Mostafa Shraief <mostafashraief5@gmail.com>
2024-11-14 16:24:47 +0300

    Add '16. Composition' Source Code

---
+++ ProgrammingAdvices/Course 16/16. Composition.cs
@@ -0,0 +1,46 @@
+using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+
+
+namespace Main
+{
+    internal class Program
+    {
+        public class clsPerson
+        {
+            public string Name { get; set; }
+
+            public clsPerson(string Name)
+            {
+                this.Name = Name;
+            }
+
+            public void Hi(string City)
+            {
+                Console.WriteLine("Hi " + Name);
+
+                clsAddress address = new clsAddress(City);
+                Console.WriteLine("City: " + City);
+            }
+        }
+
+        public class clsAddress
+        {
+            public string City { get; set; }
+
+            public clsAddress(string City)
+            {
+                this.City = City;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            clsPerson person = new clsPerson("Mostafa");
+            person.Hi("Aleppo");
+        }
+    }
+}
